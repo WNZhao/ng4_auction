71ab26636c1383a2791eebde1f4bb3bd74e69f30
willzhao <nanan_zhao@163.com>
2018-07-16 23:18:38 +0800

    product list

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
-
+  //组件初始化时调用一次
   ngOnInit() {
   }
 
 }
+
+/* 
+  定义实体类 产品信息
+*/
+
