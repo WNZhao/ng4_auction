5e51be4d6e35a680fbe487fcb427014d23fd938e
willzhao <nanan_zhao@163.com>
2018-07-26 23:40:19 +0800

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -10,6 +11,14 @@ import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StartsComponent } from './starts/starts.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule,Routes } from '@angular/router';
+
+const routeConfig:Routes=[
+    {path:'',component:HomeComponent},
+    {path:'product/:productTitle',component:ProductDetailComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,12 +28,15 @@ import { StartsComponent } from './starts/starts.component';
     SearchComponent,
     CarouselComponent,
     ProductComponent,
-    StartsComponent
+    StartsComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
