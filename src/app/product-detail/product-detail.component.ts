4129f1ec0e898d2eacabeabacbd5f0c125f05c0d
willzhao <nanan_zhao@163.com>
2018-08-14 16:23:08 +0800

    rating

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -12,12 +12,29 @@ export class ProductDetailComponent implements OnInit {
     product: Product;
     comments:Comment[];
 
+    newRating:number=5;
+    newComment:string="";
+
+    isCommentHidden:boolean=true;
+
   constructor(private routerInfo: ActivatedRoute,private productService:ProductService) { }
 
   ngOnInit() {
       let productId:number = this.routerInfo.snapshot.params["productId"]
       this.product = this.productService.getProduct(productId);
       this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment(){
+      let comment = new Comment(0,this.product.id,new Date().toISOString(),"someone",this.newRating,this.newComment);
+      this.comments.unshift(comment);
+
+      let sum = this.comments.reduce((sum,comment)=>sum+comment.rating,0);
+      this.product.rating = sum/this.comments.length;
+
+      this.newComment="";
+      this.newRating=5;
+      this.isCommentHidden=true;
   }
 
 }
